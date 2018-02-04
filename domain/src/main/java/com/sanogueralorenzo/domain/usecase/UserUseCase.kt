b007418d7e5d51c9a896b467f912e8d165333c93
sanogueralorenzo <sanogueralorenzo@gmail.com>
2018-02-04 12:18:57 +0100

    Removed unused imports

--- domain/src/main/java/com/sanogueralorenzo/domain/usecase/UserUseCase.kt
+++ domain/src/main/java/com/sanogueralorenzo/domain/usecase/UserUseCase.kt
@@ -3,7 +3,6 @@ package com.sanogueralorenzo.domain.usecase
 import com.sanogueralorenzo.domain.model.User
 import com.sanogueralorenzo.domain.repository.UserRepository
 import io.reactivex.Flowable
-import io.reactivex.Single
 import javax.inject.Inject
 
 class UserUseCase @Inject constructor(private val repository: UserRepository) : UseCase<Flowable<User>> {
