4fe9f75507695b390e0b9576b2bbac59c76660ff
sanogueralorenzo <sanogueralorenzo@gmail.com>
2018-10-03 00:47:02 +0200

    Added @field: to fix moshi temp bug

--- posts/src/main/java/com/sanogueralorenzo/posts/data/model/UserEntity.kt
+++ posts/src/main/java/com/sanogueralorenzo/posts/data/model/UserEntity.kt
@@ -4,10 +4,10 @@ import com.sanogueralorenzo.posts.domain.model.User
 import com.squareup.moshi.Json
 
 data class UserEntity(
-    @Json(name = "id") val id: String,
-    @Json(name = "name") val name: String,
-    @Json(name = "username") val username: String,
-    @Json(name = "email") val email: String
+    @field:Json(name = "id") val id: String,
+    @field:Json(name = "name") val name: String,
+    @field:Json(name = "username") val username: String,
+    @field:Json(name = "email") val email: String
 )
 
 fun UserEntity.mapToDomain(): User = User(id, name, username, email)
