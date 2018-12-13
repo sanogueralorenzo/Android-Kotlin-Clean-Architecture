ac08c1f333a3144d75aaf0564634b854bbcdc77d
sanogueralorenzo <sanogueralorenzo@gmail.com>
2018-12-13 16:22:25 +0100

    Convert to expression body

--- features/posts/src/main/java/com/sanogueralorenzo/posts/Posts.kt
+++ features/posts/src/main/java/com/sanogueralorenzo/posts/Posts.kt
@@ -2,17 +2,12 @@ package com.sanogueralorenzo.posts
 
 import org.koin.core.context.loadKoinModules
 
-/**
- * To be used by main application
- */
 object Posts {
-    fun init() {
-        loadKoinModules(
-            viewModelModule,
-            useCaseModule,
-            repositoryModule,
-            networkModule,
-            cacheModule
-        )
-    }
+    fun init() = loadKoinModules(
+        viewModelModule,
+        useCaseModule,
+        repositoryModule,
+        networkModule,
+        cacheModule
+    )
 }
