367e6019b2d2d30d164d695d0f280ddf66bc08df
sanogueralorenzo <sanogueralorenzo@gmail.com>
2020-08-05 16:15:55 -0700

    Add gradle build scan on every build

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,17 @@
+plugins {
+    // https://plugins.gradle.org/plugin/com.gradle.enterprise
+    id("com.gradle.enterprise").version("3.4")
+}
+
+gradleEnterprise {
+    buildScan {
+        // Accept the license agreement for com.gradle.build-scan plugin
+        termsOfServiceUrl = "https://gradle.com/terms-of-service"
+        termsOfServiceAgree = "yes"
+        publishAlways()
+    }
+}
+
 include(
     ":app",
     ":sample",
